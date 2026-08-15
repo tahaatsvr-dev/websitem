14d18a23d2be02528e81029862ed546db7fae17d
tahaatsvr-dev <tahaatsvr@gmail.com>
2026-08-15 17:33:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 
 @app.route("/health")
 def health():
-    return "OK", 200
+    return "Doro Lojistik aktif", 200
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
